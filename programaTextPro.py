41a48a8cd78b5751e540c9cbb61bc9bb7243571c
arturonegocios <58112295+arturonegocios@users.noreply.github.com>
2019-11-29 18:41:31 -0400

    Add files via upload

---
+++ programaTextPro.py
@@ -0,0 +1,17 @@
+listaEntrada = []
+String = ''
+String.__add__("koder")
+
+
+while True:
+    entrada =  input("Diga algo por favor: ")
+    listaEntrada.append(entrada)
+    if entrada == "/end":
+        listaEntrada.remove("/end")
+        for valor in listaEntrada:
+            if 
+            String += valor.title() + '. '
+        print(String)
+        break
+    else:
+        continue
